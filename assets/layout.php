3f97e295e32df329388e763c034755762d8cfef3
Pavel Janda <me@paveljanda.com>
2017-11-23 00:15:31 +0100

    Added some responsive css

--- assets/layout.php
+++ assets/layout.php
@@ -2,6 +2,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<title>{title}</title>
 	<style>{styles}</style>
 	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
