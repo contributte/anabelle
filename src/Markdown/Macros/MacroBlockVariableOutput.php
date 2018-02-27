ad107207490fc0e8e6056a37a95df5db146bee93
Pavel Janda <me@paveljanda.com>
2018-02-27 15:59:17 +0100

    MacroBlockVariableOutput: added automatic identation

--- src/Markdown/Macros/MacroBlockVariableOutput.php
+++ src/Markdown/Macros/MacroBlockVariableOutput.php
@@ -13,9 +13,26 @@ final class MacroBlockVariableOutput extends AbstractMacroVariable implements IM
 		 * Remove lines with inline variables definition and put then into DocuScope
 		 */
 		$content = preg_replace_callback(
-			'/\{\$\$([a-zA-Z_0-9]+)\}/m',
+			'/(^.*)?\{\$\$([a-zA-Z_0-9]+)\}/m',
 			function(array $input): string {
-				return $this->docuScope->getBlockVariable($input[1]);
+				$whitespacePrefix = '';
+
+				/**
+				 * Prefix the whole block with current identation
+				 */
+				foreach (str_split($input[1]) as $character) {
+					if ($character === "\t") {
+						$whitespacePrefix .= $character;
+
+						continue;
+					}
+
+					break;
+				}
+
+				$block = $this->docuScope->getBlockVariable($input[2]);
+
+				return $input[1] . preg_replace('/\n/m', "\n{$whitespacePrefix}", $block);
 			},
 			$content
 		);
